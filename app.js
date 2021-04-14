f24ce7cfd907b3310d254fc357485c5551dd7cef
HMashhrawi <>
2021-04-14 12:01:45 +0000

    fast&furious

---
+++ app.js
@@ -0,0 +1,15 @@
+var likeFandf= prompt('if you like fast and furious answer yes if you dont just answer no');
+
+if (likeFandf == 'yes'){
+  document.write('Welcome fast&furiousLOvers');
+  document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMTrdDQiElf3opr67Ja6FewlDg_fvVFqlhyQ&usqp=CAU">');
+
+} else{
+  alert('this website not for you');
+  document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc172nQE-DieCGRmeMVwPuz6pGWeGz-66KlQ&usqp=CAU">');
+  }
+
+ 
+
+/// favorite movie 
+
