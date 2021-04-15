078db3f9b57d877e2deae196b8b1001517068ccd
HMashhrawi <>
2021-04-15 11:48:09 +0000

    java2

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 var likeFandf= prompt('if you like fast and furious answer yes if you dont just answer no');
 
-if (likeFandf == 'yes'){
-  document.write('Welcome fast&furiousLOvers');
-  document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMTrdDQiElf3opr67Ja6FewlDg_fvVFqlhyQ&usqp=CAU">');
 
-} else{
-  alert('this website not for you');
-  document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc172nQE-DieCGRmeMVwPuz6pGWeGz-66KlQ&usqp=CAU">');
-  }
 
  
 
 /// favorite movie 
 
+/* 
+to do list: 
+ask the user if he like fast and furious movie
+validator--keep asking the user to answer with yes or no
+ask the user how many pic he/she want to see on the webpage
+
+*/
+
+
+
+// validator
+
+while(likeFandf !== 'yes'&& likeFandf !=='no'){
+  likeFandf = prompt('Yes or no Just!');
+}
+
+if(likeFandf =='yes'){
+  userAnswer='<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMTrdDQiElf3opr67Ja6FewlDg_fvVFqlhyQ&usqp=CAU">';
+
+} else if (likeFandf == 'no'){
+  var num=prompt('rate from 1 to 10 how much do hate fast and furious')
+   userAnswer='<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc172nQE-DieCGRmeMVwPuz6pGWeGz-66KlQ&usqp=CAU">';
+}
+
+
+
+var imagesNum= prompt('how many image for Fast&furious do you want??');
+
+for(var i=0 ; i<imagesNum; i++){
+ document.write(userAnswer);
+
+}
