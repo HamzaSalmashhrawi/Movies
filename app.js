137af18b1ef42037b1288bb74be2f8e4cac7acd7
HMashhrawi <>
2021-04-18 12:01:31 +0000

    lap06+07

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 
-
-
  
 
 /// favorite movie 
@@ -14,30 +12,30 @@ ask the user how many pic he/she want to see on the webpage
 */
 // function expresstion 
 
-<<<<<<< HEAD
+
 var likeFandf;
 
 var doYou= function(){
 
 likeFandf= prompt('if you like fast and furious answer yes if you dont just answer no');
-=======
-var likeFandf= prompt('if you like fast and furious answer yes if you dont just answer no');
->>>>>>> e4d7533f12e2988be3b9c8806c037f9abc48f1d6
+
+
 
 
 // validator
 
 while(likeFandf !== 'yes'&& likeFandf !=='no'){
   likeFandf = prompt('your answer should be yes or no')
-<<<<<<< HEAD
+
 }
 console.log('Fast and furious my favorite movie');
-=======
->>>>>>> e4d7533f12e2988be3b9c8806c037f9abc48f1d6
+
 }
 doYou();
 
 // function declaration
+
+var userAnswer='<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc172nQE-DieCGRmeMVwPuz6pGWeGz-66KlQ&usqp=CAU">';
 
 function picTures(){
 
